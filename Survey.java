0e5929f4d0dfd170e87269421dbda8d06413c470
Sarah Sullivan <sullytwin246@gmail.com>
2018-03-08 17:41:44 -0800

    Initial commit

---
+++ Survey.java
@@ -0,0 +1,36 @@
+import java.awt.EventQueue;
+import java.awt.Dimension;
+/**
+ * Write a description of class Survey here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Survey
+{
+    public static void main(String[] args)
+    {
+        /* It posts an event (Runnable)at the end of Swings event list and is
+        processed after all other GUI events are processed.*/
+        EventQueue.invokeLater(new Runnable()
+            {
+                public void run()
+                {
+                    //try - catch block
+                    try 
+                    {
+                        //Create object of OldWindow
+                        OldWindow frame = new OldWindow();
+                        //set frame visible true
+                        frame.setVisible(true); 
+                     
+                    } 
+                    catch (Exception e)
+                    {
+                        e.printStackTrace();
+                    }
+
+                }
+            });
+    }
+}
